2eba9cefa8eb40821d95d11b56fc776eca34e70a
johnd <johndward01@yahoo.com>
2021-01-08 16:53:07 -0600

    deleted answers on main branch

--- main.js
+++ main.js
@@ -1,62 +1,29 @@
 /*----------- Exercise #1: SELECTING/MANIPULATING ELEMENTS -----------*/
-let node1 = document.getElementById("node1");
-let node2 = document.getElementsByClassName("node2")[0];
-let h3_list = document.getElementsByTagName("h3");
 
-node1.innerHTML = "I used the getElementById('node1') method to access this";
-node2.innerHTML = "I used the getElementByClassName('node2') method to access this";
-
-for (let i = 0; i < h3_list.length; i++) {
-    h3_list[i].innerHTML = "I used the getElementByTagName('h3') method to access all the h3 tags";
-}
-
+// Select Node #1 and change the text to: "I used the getElementById("node1") method to access this" */
+// Select Node #2 and change the text to: "I used the getElementByClassName("node2") method to access this" */
+// Select ALL the h3 tags and change the text to: "I used the getElementByTagName("h3") method to access all of these" */
 
 
 /*----------- Exercise #2: CREATING/APPENDING/INSERTING ELEMENTS/OBJECTS -----------*/
-let p = document.getElementById("parent"); //parent node
 
-let para1 = document.createElement("p");
-para1.innerHTML = "Child Node #1 - This node was created using the createElement() method";
-p.appendChild(para1);
+// TODO: Create a text node using this element.createElement() and put this text inside "This node was created using the createElement() method"
+// TODO: Append the created node to the parent node using the element.appendChild() method
 
-let myNode = document.createTextNode("Child Node #2 - This node was created using the createTextNode() method");
-p.insertBefore(myNode, para1);
-
+// TODO: Create a text node using this element.createTextNode() and put this text inside "This node was created using the createTextNode() method"
+// TODO: Insert the created text node after the parent but before the one you just created using the element.insertBefore() method
 
 /*----------- Exercise #3: REMOVING/REPLACING ELEMENTS/OBJECTS -----------*/
-let parent = document.getElementById("exercise3");
-let n1 = document.getElementById("N1");
-let newChild = document.createElement("p");
-newChild.textContent = "New Child Node";
-newChild.style.textAlign = "center";
-n1 = parent.replaceChild(newChild, n1);
-// parent.replaceChild(n1, temp);
 
-let node = document.getElementById("exercise3");
-if (node.parentNode) {
-    node.parentNode.removeChild(node);
-}
-
+// TODO: Replace the "Child Node" with the "New Child Node"
+// TODO: Remove the "New Child Node"
 
 /*----------- Exercise #4: ANIMATIONS ----------- */
-let pos = 0;
-var box = document.getElementById("box");
-var t = setInterval(move, 10);
 
-function move() {
-    if (pos >= 150) {
-        clearInterval(t);
-    } else {
-        pos += 1;
-        box.style.left = pos + "px";
-    }
-}
-
-/* BONUS - Make the red box go all the way around the perimeter of the green box */
+// TODO: Write your JavaScript here to make the red box go from right to left
+// BONUS - Make the red box go all the way around the perimeter of the green box */
 
 /*----------- Exercise #5: DOM EVENTS --------------*/
-function show() {
-    alert("Clicking the button triggers the onclick event, which calls the JS function show()... which alerts the user");
-}
 
-let btn = document.getElementById("")
+// TODO: write a function called "show" which alerts the user this message -> "Clicking the button triggers the onclick event, which calls the JS function show()... which alerts the user"
+
