6602c5cc37162e7a2daf33a6071e1e1f2bf121ed
Ben <bbryant@truecoders.io>
2021-02-01 17:34:55 -0600

    Added ex 1-3 solutions

--- main.js
+++ main.js
@@ -1,23 +1,39 @@
 /*----------- Exercise #1: SELECTING/MANIPULATING ELEMENTS -----------*/
 
 // Select Node #1 and change the text to: "I used the getElementById("node1") method to access this"
-// Select Node #2 and change the text to: "I used the getElementByClassName("node2") method to access this" */
-// Select ALL the h3 tags and change the text to: "I used the getElementByTagName("h3") method to access all of these" */
-
+document.getElementById("node1").innerText =
+  'I used the getElementById("node1") method to access this';
+// Select Node #2 and change the text to: "I used the getElementByClassName("node2") method to access this"
+document.getElementsByClassName("node2")[0].innerText =
+  'I used the getElementsByClassName("node2") to access this';
+// Select ALL the h3 tags and change the text to: "I used the getElementByTagName("h3") method to access all of these"
+const h3Nodes = document.getElementsByTagName("h3");
+for (let node of h3Nodes) {
+  node.innerText =
+    'I used the getElementByTagName("h3") method to access all of these';
+}
 
 /*----------- Exercise #2: CREATING/APPENDING/INSERTING ELEMENTS/OBJECTS -----------*/
 
-// TODO: Create a text node using this element.createElement() and put this text inside "This node was created using the createElement() method"
+// TODO: Create a text node using this element.createElement() and put this text inside "I am a p element"
+const p = document.createElement("p");
+p.innerText = "I am a p element";
 // TODO: Append the created node to the parent node using the element.appendChild() method
-
-// TODO: Create a text node using this element.createTextNode() and put this text inside "This node was created using the createTextNode() method"
-// TODO: Insert the created text node after the parent but before the one you just created using the element.insertBefore() method
-
+const parent = document.getElementById("parent");
+parent.appendChild(p);
+// TODO: Create a 'small' element using this element.createTextNode() and put this text inside "I am small"
+const small = document.createElement("small");
+small.innerText = "I am small";
+// TODO: Insert the created element inside the parent but before the one you just created using the element.insertBefore() method
+parent.insertBefore(small, p);
 /*----------- Exercise #3: REMOVING/REPLACING ELEMENTS/OBJECTS -----------*/
 
-// TODO: Replace the "Child Node" with the "New Child Node"
-// TODO: Remove the "New Child Node"
-
+// TODO: Replace the "Child Node" with a newly created element with text content: "New Child Node"
+const newP = document.createElement("p");
+newP.innerText = "New Child Node";
+// TODO: Remove the "p"
+const exercise3 = document.getElementById("exercise3");
+exercise3.replaceChild(newP, document.getElementById("oldNode"));
 /*----------- Exercise #4: ANIMATIONS ----------- */
 
 // TODO: Write your JavaScript here to make the red box go from right to left
@@ -26,4 +42,3 @@
 /*----------- Exercise #5: DOM EVENTS --------------*/
 
 // TODO: write a function called "show" which alerts the user this message -> "Clicking the button triggers the onclick event, which calls the JS function show()... which alerts the user"
-
