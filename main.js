b0ebb66c722ea7c8f73ea6c6a4eb34a71d6d9a60
Ben Bryant <brbryant2639@gmail.com>
2021-11-03 10:48:29 -0500

    Style updates

---
+++ main.js
@@ -0,0 +1,39 @@
+console.log("Script attached");
+/*----------- Exercise #1: SELECTING/MANIPULATING ELEMENTS -----------*/
+
+// Select Node #1 and change the text to: "I used the getElementById("node1") method to access this"
+
+// Select Node #2 and change the text to: "I used the getElementByClassName("node2") method to access this" */
+
+// Select ALL the h3 tags and change the text to: "I used the getElementByTagName("h3") method to access all of these" */
+
+/*----------- Exercise #2: CREATING/APPENDING/INSERTING ELEMENTS/OBJECTS -----------*/
+
+// TODO: Create a paragraph element using this element.createElement() and put this text inside "This node was created using the createElement() method"
+
+// TODO: Append the created node to the parent node using the element.appendChild() method
+
+// TODO: Create a <a> element using this element.createElement() and put this text inside "I am a <a> tag"
+
+// BONUS: Add a link href to the <a>
+
+// TODO: Insert the created <a> in the parent but before the <p> you just created using the element.insertBefore() method
+
+/*----------- Exercise #3: REMOVING/REPLACING ELEMENTS/OBJECTS -----------*/
+
+// TODO: Replace the "Child Node" with a new <p> element that reads "New Child Node"
+
+// TODO: Remove the "New Child Node"
+
+/*----------- Exercise #4: ANIMATIONS ----------- */
+// TODO: Write your JavaScript here to make the red box go from left to right
+// let interval = setInterval(move, 10);
+
+// BONUS - Make the red box go all the way around the perimeter of the green box
+
+/*----------- Exercise #5: DOM EVENTS --------------*/
+
+// TODO: write a function called "show" which creates a new div with an alerting message to the user with this message
+// -> "Clicking the button triggers the onclick event, which calls the JS function show()... which alerts the user"
+// This div should be a 'modal' that covers the main content on the screen
+// BONUS: The modal popup should be able to be closed. Refactor for this functionality
